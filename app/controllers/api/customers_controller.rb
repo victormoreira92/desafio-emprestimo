6cd0fb58c62aeac87b79231944c0d994e1742553
Victor Hugo Moreira <vhmrj92@gmail.com>
2025-04-01 20:47:28 -0300

    feat: Retorno de json

--- app/controllers/api/customers_controller.rb
+++ app/controllers/api/customers_controller.rb
@@ -6,16 +6,24 @@ module Api
       @customer = Customer.new(customer_params)
 
       if @customer.save
-        render json: @customer.loans, status: :created
+        render json: { customer: @customer.name,
+                       loans: @customer.loans.map { |loan| loan_attributes(loan) } }, status: :created
       else
-        render json: @customer.errors, status: :unprocessable_entity
+        render json: { error: @customer.errors }, status: :unprocessable_entity
       end
     end
-
+    
     private
 
       def customer_params
         params.require(:customer).permit(:age, :name, :cpf, :income, :location)
+      end
+
+      def loan_attributes(loan)
+        {
+          type: loan.type_loan.upcase,
+          interest_rate: loan.interest_rate
+        }
       end
   end
 end
